9e826304a0736bec8635caf3639944e4008c81e1
rajankarmakar <rajankarmakar@hotmail.com>
2024-06-04 17:41:46 +0600

    Add home page and theme cleanup

---
+++ assets/theme.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+      autoplay: true,
+      smartSpeed: 1000,
+      loop: false,
+      nav: false,
+      dots: false,
+      items: 1,
+      dotsData: true,
+    });
+  });
+})(jQuery);
